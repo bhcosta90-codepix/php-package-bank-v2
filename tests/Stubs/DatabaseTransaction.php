b15031416db3f49faf5e4f33aa243181753663dc
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-26 19:44:00 -0300

    wip

--- tests/Stubs/DatabaseTransaction.php
+++ tests/Stubs/DatabaseTransaction.php
@@ -7,11 +7,11 @@ namespace Tests\Stubs;
 use BRCas\CA\Contracts\Transaction\DatabaseTransactionInterface;
 use Closure;
 
-class DatabaseTransactionInterface implements DatabaseTransactionInterface
+class DatabaseTransaction implements DatabaseTransactionInterface
 {
-    public function transaction(Closure $closure): void
+    public function transaction(Closure $closure): mixed
     {
-        $closure();
+        return $closure();
     }
 
 }
